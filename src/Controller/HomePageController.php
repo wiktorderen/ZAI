b37b624cc2f90da9711a98120737b0fb559d7897
Ubuntu <wiktor1474@wp.pl>
2022-04-27 02:03:25 -0700

    gitignore_update_&_EAB_install

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class IndexController extends AbstractController
+class HomePageController extends AbstractController
 {
     /**
-     * @Route("/index", name="app_index")
+     * @Route("/", name="app_home_page")
      */
     public function index(): Response
     {
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+        return $this->render('home_page/index.html.twig', [
+            'controller_name' => 'HomePageController',
         ]);
     }
 }
