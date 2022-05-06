e5b8b9302982cb956140d37913ab7eb63392426d
Ubuntu <wiktor1474@wp.pl>
2022-05-06 02:52:25 -0700

    weather_api_showing_temperature

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class HomePageController extends AbstractController
 {
@@ -13,8 +14,20 @@ class HomePageController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('home_page/index.html.twig', [
-            'controller_name' => 'HomePageController',
-        ]);
+        $city_name = 'Olsztyn';
+        $api_key = '702160109552cbfcf97527f0449b8760';
+        $api_url = 'https://api.openweathermap.org/data/2.5/weather?lat=53.775275&lon=20.486707&appid=' .$api_key;
+        $weather = json_decode( file_get_contents(($api_url), true));
+
+        $weather_json = json_encode($weather);
+        $temperature = $weather->main->temp;
+        $celcius = round($temperature - 273.15);
+        //$current_temp = $weather['weather'][0]['main'];
+        //$current_desc = $weather[0]['description'];
+
+
+        return $this->render('home_page/index.html.twig',
+            array('celcius' => $celcius)
+        );
     }
 }
